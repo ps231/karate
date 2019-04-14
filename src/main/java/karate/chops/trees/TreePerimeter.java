0b8bb462df31f2d9a8c1309b9814474193416b12
Prasad Shindikar <prasad.shindikar@nordstrom.com>
2019-04-14 15:18:23 -0700

    try tree perimeter

--- src/main/java/karate/chops/trees/TreePerimeter.java
+++ src/main/java/karate/chops/trees/TreePerimeter.java
@@ -1,110 +1,111 @@
 package karate.chops.trees;
 
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class TreePerimeter {
-	
-	public static void main(String...strings) {
-		TreePerimeter p = new TreePerimeter();
-		p.printTreePerimeter(createCustomBinaryTree());
-	}
-	
-	private static BinaryTreeNode createCustomBinaryTree() {BinaryTreeNode root = new BinaryTreeNode();
-    root.value = 3;
 
-    BinaryTreeNode left = new BinaryTreeNode();
-    left.value = 5;
-
-    BinaryTreeNode right = new BinaryTreeNode();
-    right.value = 1;
-
-    root.left = left;
-    root.right = right;
-
-    left = new BinaryTreeNode();
-    left.value = 6;
-
-    right = new BinaryTreeNode();
-    right.value = 2;
-
-    root.left.left = left;
-    root.left.right = right;
-
-    left = new BinaryTreeNode();
-    left.value = 7;
-    
-    right = new BinaryTreeNode();
-    right.value = 4;
-    
-    root.left.right.left = left;
-    root.left.right.right = right;
-    
-    left = new BinaryTreeNode();
-    left.value = 0;
-
-    right = new BinaryTreeNode();
-    right.value = 8;
-
-    root.right.left = left;
-    root.right.right = right;
-    
-    left = new BinaryTreeNode();
-    left.value = 22;
-    
-    right = new BinaryTreeNode();
-    right.value = 15;
-    
-    root.right.right.left = left;
-    root.right.right.right = right;
-    
-    left = new BinaryTreeNode();
-    left.value = 35;
-    
-    right = new BinaryTreeNode();
-    right.value = 50;
-    
-    root.right.right.left.left = left;
-    root.right.right.right.right = right;
-    
-    return root;
+    public static void main(String... strings) {
+        TreePerimeter p = new TreePerimeter();
+        p.printTreePerimeter(createCustomBinaryTree());
     }
 
-	private void printTreePerimeter(BinaryTreeNode root){
-		Set<Integer> perimeter = new LinkedHashSet<Integer>();
-		BinaryTreeNode iter = root;
-		addLeftPerimeter(iter, perimeter);
-		iter=root;
-		addLeaves(iter, perimeter);
-		iter=root;
-		addRightPerimeter(iter, perimeter);
-		printPerimeter(perimeter);
-	}
+    private static BinaryTreeNode createCustomBinaryTree() {
+        BinaryTreeNode root = new BinaryTreeNode();
+        root.value = 3;
 
-	private void addLeftPerimeter(BinaryTreeNode root, Set<Integer> perimeter){
-		while(root!=null){
-			perimeter.add(root.value);
-			root = root.left;
-		}
-	}
+        BinaryTreeNode left = new BinaryTreeNode();
+        left.value = 5;
 
-	private void addLeaves(BinaryTreeNode root, Set<Integer> perimeter){
-		if(root == null)
-			return;
-		addLeaves(root.left, perimeter);
-		addLeaves(root.right, perimeter);
+        BinaryTreeNode right = new BinaryTreeNode();
+        right.value = 1;
 
-		if(root.left == null && root.right == null)
-			perimeter.add(root.value);
-	}
+        root.left = left;
+        root.right = right;
 
-	private void addRightPerimeter(BinaryTreeNode root, Set<Integer> perimeter){
-		if(root == null)
-			return;
-		addRightPerimeter(root.right, perimeter);
-		perimeter.add(root.value);
-	}
+        left = new BinaryTreeNode();
+        left.value = 6;
 
-	private void printPerimeter(Set<Integer> perimeter){
-		perimeter.stream().forEach(System.out::println);
-	}
+        right = new BinaryTreeNode();
+        right.value = 2;
+
+        root.left.left = left;
+        root.left.right = right;
+
+        left = new BinaryTreeNode();
+        left.value = 7;
+
+        right = new BinaryTreeNode();
+        right.value = 4;
+
+        root.left.right.left = left;
+        root.left.right.right = right;
+
+        left = new BinaryTreeNode();
+        left.value = 0;
+
+        right = new BinaryTreeNode();
+        right.value = 8;
+
+        root.right.left = left;
+        root.right.right = right;
+
+        left = new BinaryTreeNode();
+        left.value = 22;
+
+        right = new BinaryTreeNode();
+        right.value = 15;
+
+        root.right.right.left = left;
+        root.right.right.right = right;
+
+        left = new BinaryTreeNode();
+        left.value = 35;
+
+        right = new BinaryTreeNode();
+        right.value = 50;
+
+        root.right.right.left.left = left;
+        root.right.right.right.right = right;
+
+        return root;
+    }
+
+    private static void printTreePerimeter(BinaryTreeNode n) {
+        List<Integer> treePerimeter = new ArrayList<>();
+        getLeftPerimeter(n, treePerimeter);
+        getLeaves(n, treePerimeter);
+        getRightPerimeter(n, treePerimeter);
+        treePerimeter.stream().forEach(i -> System.out.print(i + " "));
+    }
+
+    private static void getRightPerimeter(BinaryTreeNode n, List<Integer> treePerimeter) {
+        if (n.right != null)
+            n = n.right;
+        List<Integer> rightPerimeter = new ArrayList<>();
+        while (n.right != null) {
+            rightPerimeter.add(n.value);
+            n = n.right;
+        }
+        Collections.reverse(rightPerimeter);
+        treePerimeter.addAll(rightPerimeter);
+    }
+
+    private static void getLeaves(BinaryTreeNode n, List<Integer> treePerimeter) {
+        if (n == null)
+            return;
+        if (n.left == null && n.right == null)
+            treePerimeter.add(n.value);
+        getLeaves(n.left, treePerimeter);
+        getLeaves(n.right, treePerimeter);
+    }
+
+    private static void getLeftPerimeter(BinaryTreeNode n, List<Integer> treePerimeter) {
+        while (n.left != null) {
+            treePerimeter.add(n.value);
+            n = n.left;
+        }
+    }
 }
