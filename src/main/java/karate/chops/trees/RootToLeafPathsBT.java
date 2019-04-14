2c709c677a58c369724f4291810ceec843dffd51
Prasad Shindikar <prasad.shindikar@nordstrom.com>
2019-04-14 12:00:10 -0700

    try root to leaf paths

--- src/main/java/karate/chops/trees/RootToLeafPathsBT.java
+++ src/main/java/karate/chops/trees/RootToLeafPathsBT.java
@@ -3,45 +3,33 @@ package karate.chops.trees;
 import java.util.*;
 
 public class RootToLeafPathsBT {
-//	public static void main(String...strings ) {
-//		int[] path = new int[5];
-//		printRToLPaths(BinaryTree.createPerfectBinaryTree(),path,0);
-//	}
-//
-//	private static void printRToLPaths(BinaryTreeNode n, int[] path, int i) {
-//		// TODO Auto-generated method stub
-//		if(n==null) return;
-//		path[i++]=n.value;
-//		printRToLPaths(n.left, path,i);
-//		printRToLPaths(n.right, path,i);
-//		
-//		if(n.left == null && n.right == null)
-//			for(int j = 0; j<i;j++)
-//				System.out.println(path[j]);
-//	}
+    public static void main(String[] args) {
+        printAllRootToLeafPaths(BinaryTree.createPerfectBinaryTree());
+        printAllRootToLeafPaths(BinaryTree.createBalancedAsymmetricTree());
+        printAllRootToLeafPaths(BinaryTree.createLeftSkewedTree());
+    }
 
-	public static void main(String[] args) {
-		printAllRootToLeafPaths(BinaryTree.createPerfectBinaryTree());
-		printAllRootToLeafPaths(BinaryTree.createBalancedAsymmetricTree());
-		printAllRootToLeafPaths(BinaryTree.createLeftSkewedTree());
-	}
+    private static void printAllRootToLeafPaths(BinaryTreeNode root) {
+        helper(root, new ArrayList<Integer>());
+    }
 
-	private static void printAllRootToLeafPaths(BinaryTreeNode root) {
-		List<String> paths = new ArrayList<String>();
-		helper(root, paths, "");
-		paths.stream().forEach(System.out::println);
-	}
+    private static void helper(BinaryTreeNode n, List<Integer> intermediate) {
+        if (n == null)
+            return;
 
-	private static void helper(BinaryTreeNode root, List<String> paths, String intermediate) {
-		if (root == null)
-			return;
+        if (n.left == null && n.right == null) {
+            print(intermediate, n.value);
+            return;
+        }
 
-		if (root.left == null && root.right == null) {
-			paths.add(intermediate + " " + root.value);
-			return;
-		}
+        intermediate.add(n.value);
+        helper(n.left, intermediate);
+        helper(n.right, intermediate);
+        intermediate.remove(intermediate.size() - 1);
+    }
 
-		helper(root.left, paths, intermediate + " " + root.value);
-		helper(root.right, paths, intermediate + " " + root.value);
-	}
+    private static void print(List<Integer> intermediate, int val) {
+        intermediate.stream().forEach(i -> System.out.print(i + " "));
+        System.out.println(val);
+    }
 }
