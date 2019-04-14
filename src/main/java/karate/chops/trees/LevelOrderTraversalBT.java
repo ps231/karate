73aebfabdd5c73f1cc06a4be0448052c59dc0d14
Prasad Shindikar <prasad.shindikar@nordstrom.com>
2019-04-14 12:24:39 -0700

    try level order printing

--- src/main/java/karate/chops/trees/LevelOrderTraversalBT.java
+++ src/main/java/karate/chops/trees/LevelOrderTraversalBT.java
@@ -4,35 +4,39 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class LevelOrderTraversalBT {
-	
-	public static void main(String...strings ) {
-		levelOrderPrinting(BinaryTree.createSkewedBalancedTree());
-	}
 
-	private static void levelOrderPrinting(BinaryTreeNode node) {
-		// TODO Auto-generated method stub
-		if(node == null)return;
-		
-		Queue<BinaryTreeNode> q1 = new LinkedList<BinaryTreeNode>();
-		Queue<BinaryTreeNode> q2 = new LinkedList<>();
-		
-		q1.add(node);
-		
-		while(!q1.isEmpty() || !q2.isEmpty()) {
-			while(!q1.isEmpty()) {
-				BinaryTreeNode n = q1.poll();
-				System.out.print(n.value + " ");
-				if(n.left!=null) q2.offer(n.left);
-				if(n.right!=null)q2.offer(n.right);
-			}
-			System.out.println("");
-			while(!q2.isEmpty()) {
-				BinaryTreeNode n = q2.poll();
-				System.out.print(n.value + " ");
-				if(n.left!=null) q1.offer(n.left);
-				if(n.right!=null)q1.offer(n.right);
-			}
-			System.out.println("");
-		}
-	}
+    public static void main(String... strings) {
+        levelOrderPrinting(BinaryTree.createPerfectBinaryTree());
+        levelOrderPrinting(BinaryTree.createLeftSkewedTree());
+        levelOrderPrinting(BinaryTree.createSkewedBalancedTree());
+    }
+
+    private static void levelOrderPrinting(BinaryTreeNode n) {
+        if (n == null)
+            return;
+        Queue<BinaryTreeNode> q1 = new LinkedList<>();
+        Queue<BinaryTreeNode> q2 = new LinkedList<>();
+
+        q1.offer(n);
+
+        while (!q1.isEmpty() || !q2.isEmpty()) {
+            traverseLevel(q1, q2);
+            traverseLevel(q2, q1);
+        }
+    }
+
+    private static void traverseLevel(Queue<BinaryTreeNode> currentLevel, Queue<BinaryTreeNode> nextLevel) {
+        while (!currentLevel.isEmpty()) {
+            BinaryTreeNode t = currentLevel.poll();
+            offerNonNullNode(t.left, nextLevel);
+            offerNonNullNode(t.right, nextLevel);
+            System.out.print(t.value + " ");
+        }
+        System.out.println("");
+    }
+
+    private static void offerNonNullNode(BinaryTreeNode n, Queue<BinaryTreeNode> q) {
+        if (n != null)
+            q.offer(n);
+    }
 }
