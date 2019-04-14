6a6497af90bda9a41464d16fa398e36a55f476f8
Prasad Shindikar <prasad.shindikar@nordstrom.com>
2019-04-13 18:39:31 -0700

    another try

--- src/main/java/karate/chops/trees/FindHeightofBT.java
+++ src/main/java/karate/chops/trees/FindHeightofBT.java
@@ -1,18 +1,15 @@
 package karate.chops.trees;
 
 public class FindHeightofBT {
-	
-	public static void main(String[] args) {
-		System.out.println(findHeight(BinaryTree.createPerfectBinaryTree()));
-		System.out.println(findHeight(BinaryTree.createLeftSkewedTree()));
-	}
-	
-	private static int findHeight(BinaryTreeNode n) {
-		if(n==null)
-			return 0;
-		int leftHeight = findHeight(n.left);
-		int rightHeight = findHeight(n.right);
-		return 1 + Math.max(leftHeight, rightHeight);
-	}
 
+    public static void main(String[] args) {
+        System.out.println(findHeight(BinaryTree.createPerfectBinaryTree()));
+        System.out.println(findHeight(BinaryTree.createLeftSkewedTree()));
+    }
+
+    private static int findHeight(BinaryTreeNode n) {
+        if (n == null)
+            return 0;
+        return 1 + Math.max(findHeight(n.left), findHeight(n.right));
+    }
 }
