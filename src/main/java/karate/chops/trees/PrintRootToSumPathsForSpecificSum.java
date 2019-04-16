1838a4dddbebd64c1150a8764320c6a1aef59ce9
Prasad Shindikar <prasad.shindikar@nordstrom.com>
2019-04-15 17:12:20 -0700

    extract the paths out so that they can be counted, decorated etc

--- src/main/java/karate/chops/trees/PrintRootToSumPathsForSpecificSum.java
+++ src/main/java/karate/chops/trees/PrintRootToSumPathsForSpecificSum.java
@@ -1,22 +1,30 @@
 package karate.chops.trees;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrintRootToSumPathsForSpecificSum {
     public static void main(String[] args) {
-        printPathThatHasExactSum(BinaryTree.createPerfectBinaryTree(), 10);
+        BinaryTreeNode n = BinaryTree.createPerfectBinaryTree();
+        n.left.left.value = 7;
+        printPathThatHasExactSum(n, 10);
         printPathThatHasExactSum(BinaryTree.createPerfectBinaryTree(), 11);
         printPathThatHasExactSum(BinaryTree.createPerfectBinaryTree(), 6);
     }
 
     private static void printPathThatHasExactSum(BinaryTreeNode n, int sum) {
-        helper(n, sum, "");
+        List<String> result = new ArrayList<>();
+        helper(n, sum, "", result);
+        System.out.println("Paths equal to sum: " + result.size());
+        result.stream().forEach(System.out::println);
     }
 
-    private static void helper(BinaryTreeNode n, int sum, String intermediate) {
+    private static void helper(BinaryTreeNode n, int sum, String intermediate, List<String> result) {
         if (n == null)
             return;
         if (n.left == null && n.right == null && n.value == sum)
-            System.out.println(intermediate + " " + n.value);
-        helper(n.left, sum - n.value, intermediate + " " + n.value);
-        helper(n.right, sum - n.value, intermediate + " " + n.value);
+            result.add(intermediate + " " + n.value);
+        helper(n.left, sum - n.value, intermediate + " " + n.value, result);
+        helper(n.right, sum - n.value, intermediate + " " + n.value, result);
     }
 }
