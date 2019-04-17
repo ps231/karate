21ed093b316151cd34072c42c0d14361c79b3d2c
Prasad Shindikar <prasad.shindikar@nordstrom.com>
2019-04-17 14:30:16 -0700

    try finding matching permutations from dictionary

--- src/main/java/karate/chops/recursion/FindPermutationsFromDictionary.java
+++ src/main/java/karate/chops/recursion/FindPermutationsFromDictionary.java
@@ -4,42 +4,49 @@ import java.util.*;
 
 public class FindPermutationsFromDictionary {
 
-	public static void main(String... strings) {
-		FindPermutationsFromDictionary g = new FindPermutationsFromDictionary();
-		Set<String> dictionary = new HashSet<String>();
-		dictionary.add("afi");
-		dictionary.add("ceh");
-		dictionary.add("bei");
-		dictionary.add("adi");
-		dictionary.add("bdg");
-		dictionary.add("a");
-		dictionary.add("be");
-		dictionary.add("cfh");
-		dictionary.add("cf");
-		g.generatePermutations("234", dictionary);
-	}
+    public static void main(String... strings) {
+        FindPermutationsFromDictionary g = new FindPermutationsFromDictionary();
+        Set<String> dictionary = getDictionary();
+        g.generatePermutations("234", dictionary);
+    }
 
-	private void generatePermutations(String phoneNumber, Set<String> dictionary) {
-		List<String> combinations = new ArrayList<>();
-		Map<Character, String> keypad = new HashMap<Character, String>();
-		keypad.put('2', "abc");
-		keypad.put('3', "def");
-		keypad.put('4', "ghi");
+    private void generatePermutations(String phoneNumber, Set<String> dictionary) {
+        List<String> foundPermutations = new ArrayList<>();
+        permutationHelper(phoneNumber, initializeKeypad(), dictionary, foundPermutations, 0, "");
+        foundPermutations.stream().forEach(System.out::println);
+    }
 
-		permutationHelper(phoneNumber, keypad, new char[phoneNumber.length()], 0, combinations, dictionary);
-		combinations.stream().forEach(System.out::println);
-	}
+    private void permutationHelper(String phoneNumber, Map<Character, String> keypad, Set<String> dictionary, List<String> foundPermutations, int level, String intermediate) {
+        if (level == phoneNumber.length()) {
+            if (dictionary.contains(intermediate)) {
+                foundPermutations.add(intermediate);
+            }
+            return;
+        }
+        for (int i = 0; i < phoneNumber.length(); i++) {
+            permutationHelper(phoneNumber, keypad, dictionary, foundPermutations, level + 1, intermediate + keypad.get(phoneNumber.charAt(level)).charAt(i));
+        }
+    }
 
-	private void permutationHelper(String phNo, Map<Character, String> keypad, char[] temp, int level,
-			List<String> combinations, Set<String> dictionary) {
-		if (phNo.length() == level) {
-			if(dictionary.contains(String.valueOf(temp)))
-				combinations.add(String.valueOf(temp));
-		} else {
-			for (int i = 0; i < keypad.get(phNo.charAt(level)).length(); i++) {
-				temp[level] = keypad.get(phNo.charAt(level)).charAt(i);
-				permutationHelper(phNo, keypad, temp, level + 1, combinations, dictionary);
-			}
-		}
-	}
+    private Map<Character, String> initializeKeypad() {
+        Map<Character, String> keypad = new HashMap<Character, String>();
+        keypad.put('2', "abc");
+        keypad.put('3', "def");
+        keypad.put('4', "ghi");
+        return keypad;
+    }
+
+    private static Set<String> getDictionary() {
+        Set<String> dictionary = new HashSet<String>();
+        dictionary.add("afi");
+        dictionary.add("ceh");
+        dictionary.add("bei");
+        dictionary.add("adi");
+        dictionary.add("bdg");
+        dictionary.add("a");
+        dictionary.add("be");
+        dictionary.add("cfh");
+        dictionary.add("cf");
+        return dictionary;
+    }
 }
