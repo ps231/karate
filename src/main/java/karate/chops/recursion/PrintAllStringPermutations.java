6ad225bf15ed0a86c39bff9af478ee73ce7f2f40
Prasad Shindikar <prasad.shindikar@nordstrom.com>
2019-04-17 14:48:30 -0700

    try printing all permutations of a string

--- src/main/java/karate/chops/recursion/PrintAllStringPermutations.java
+++ src/main/java/karate/chops/recursion/PrintAllStringPermutations.java
@@ -3,34 +3,29 @@ package karate.chops.recursion;
 import java.util.*;
 
 public class PrintAllStringPermutations {
-	public static void main(String[] args) {
-		printAllPermutations("abc")
-			.stream()
-			.forEach(System.out::println);
-	}
+    public static void main(String[] args) {
+        printAllPermutations("abc")
+                .stream()
+                .forEach(System.out::println);
+    }
 
-	private static List<String> printAllPermutations(String src) {
-		List<String> result = new ArrayList<String>();
-		String intermediate = "";
-		boolean[] visited = new boolean[src.length()];
-		permutationsHelper(src, result, intermediate, visited, 0);
-		return result;
-	}
+    private static List<String> printAllPermutations(String src) {
+        List<String> permutations = new ArrayList<>();
+        helper(src, permutations, 0, "", new boolean[src.length()]);
+        return permutations;
+    }
 
-	private static void permutationsHelper(String src, List<String> result, String intermediate, boolean[] visited,
-			int level) {
-		if (src.length() == intermediate.length()) {
-			result.add(intermediate);
-			return;
-		}
-
-		for (int i = 0; i < src.length(); i++) {
-			if (visited[i])
-				continue;
-
-			visited[i] = true;
-			permutationsHelper(src, result, intermediate + src.charAt(i), visited, level + 1);
-			visited[i] = false;
-		}
-	}
+    private static void helper(String src, List<String> permutations, int level, String intermediate, boolean[] visited) {
+        if (level == src.length()) {
+            permutations.add(intermediate);
+            return;
+        }
+        for (int i = 0; i < src.length(); i++) {
+            if (visited[i])
+                continue;
+            visited[i] = true;
+            helper(src, permutations, level + 1, intermediate + src.charAt(i), visited);
+            visited[i] = false;
+        }
+    }
 }
