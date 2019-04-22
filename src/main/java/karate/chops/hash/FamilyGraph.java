d74312813e7b29d38982d87813fb66c92e7ba136
Prasad Shindikar <prasad.shindikar@nordstrom.com>
2019-04-22 16:38:42 -0700

    clear up some of the implementation

--- src/main/java/karate/chops/hash/FamilyGraph.java
+++ src/main/java/karate/chops/hash/FamilyGraph.java
@@ -1,8 +1,6 @@
 package karate.chops.hash;
 
-
 import java.util.*;
-
 
 // Parents are above children:
 
@@ -22,12 +20,54 @@ import java.util.*;
 // hasCommonAncestor(parentChildPairs, 1, 3) => false
 
 class FamilyGraph {
+    public static void main(String[] args) {
+        // { parent, child }
+        int[][] parentChildPairs = new int[][]{
+                {1, 3}, {2, 3}, {3, 6}, {5, 6}, {5, 7},
+                {4, 5}, {4, 8}, {8, 10}
+        };
 
+        FamilyResults results = findNodesWithZeroAndOneParents(parentChildPairs);
+        System.out.println(results.individualsWithZeroParents);
+        System.out.println(results.individualsWithOneParent);
+
+        System.out.println(hasCommonAncestor(parentChildPairs, 6, 8));
+        System.out.println(hasCommonAncestor(parentChildPairs, 1, 3));
+        System.out.println(hasCommonAncestor(parentChildPairs, 5, 8));
+        System.out.println(hasCommonAncestor(parentChildPairs, 8, 3));
+    }
+
+    private static class FamilyResults {
+        List<Integer> individualsWithZeroParents;
+        List<Integer> individualsWithOneParent;
+        //getter,setters
+    }
+
+    private static FamilyResults findNodesWithZeroAndOneParents(int[][] parentChildPairs) {
+        Map<Integer, List<Integer>> familyMap = createFamilyMap(parentChildPairs);
+        FamilyResults results = new FamilyResults();
+        List<Integer> zeroParentIndividuals = new ArrayList<>();
+        List<Integer> oneParentIndividuals = new ArrayList<>();
+
+        for (int person : familyMap.keySet()) {
+            if (familyMap.get(person).size() == 0)
+                zeroParentIndividuals.add(person);
+            if (familyMap.get(person).size() == 1)
+                oneParentIndividuals.add(person);
+        }
+        results.individualsWithZeroParents = zeroParentIndividuals;
+        results.individualsWithOneParent = oneParentIndividuals;
+
+        return results;
+    }
 
     private static boolean hasCommonAncestor(int[][] parentChildPairs, int p1, int p2) {
+        Map<Integer, List<Integer>> familyMap = createFamilyMap(parentChildPairs);
+        return getIntersection(findAncestor(familyMap, p1), findAncestor(familyMap, p2));
+    }
 
+    private static Map<Integer, List<Integer>> createFamilyMap(int[][] parentChildPairs) {
         Map<Integer, List<Integer>> familyMap = new HashMap<>();
-
         for (int[] pair : parentChildPairs) {
             if (!familyMap.containsKey(pair[1])) {
                 familyMap.put(pair[1], new ArrayList<Integer>());
@@ -40,26 +80,17 @@ class FamilyGraph {
                 familyMap.put(pair[0], new ArrayList<Integer>());
             }
         }
-
-        List<Integer> p1Ancestors = findAncestor(familyMap, p1);
-        List<Integer> p2Ancestors = findAncestor(familyMap, p2);
-
-        boolean foundAncestor = getIntersection(p1Ancestors, p2Ancestors);
-        return foundAncestor;
+        return familyMap;
     }
-
 
     private static boolean getIntersection(List<Integer> p1, List<Integer> p2) {
         Set<Integer> s1 = new HashSet<>(p1);
-
         for (int i : p2) {
             if (s1.contains(i))
                 return true;
         }
-
         return false;
     }
-
 
     private static List<Integer> findAncestor(Map<Integer, List<Integer>> familyMap, int p) {
         List<Integer> ancestors = new ArrayList<>();
@@ -76,74 +107,4 @@ class FamilyGraph {
             helper(familyMap, familyMap.get(p).get(i), ancestors);
         }
     }
-
-    // Your function here:
-
-    private static FamilyResults findNodesWithZeroAndOneParents(int[][] parentChildPairs) {
-
-        //check for input
-
-        Map<Integer, List<Integer>> familyMap = new HashMap<>();
-
-        for (int[] pair : parentChildPairs) {
-            if (!familyMap.containsKey(pair[1])) {
-                familyMap.put(pair[1], new ArrayList<Integer>());
-            }
-            List<Integer> existingParents = familyMap.get(pair[1]);
-            existingParents.add(pair[0]);
-            familyMap.put(pair[1], existingParents);
-
-            if (!familyMap.containsKey(pair[0])) {
-                familyMap.put(pair[0], new ArrayList<Integer>());
-            }
-        }
-
-        FamilyResults results = new FamilyResults();
-
-        List<Integer> zeroParentIndividuals = new ArrayList<>();
-        List<Integer> oneParentIndividuals = new ArrayList<>();
-
-        for (int person : familyMap.keySet()) {
-            if (familyMap.get(person).size() == 0)
-                zeroParentIndividuals.add(person);
-            if (familyMap.get(person).size() == 1)
-                oneParentIndividuals.add(person);
-        }
-        results.individualsWithZeroParents = zeroParentIndividuals;
-        results.individualsWithOneParent = oneParentIndividuals;
-
-        return results;
-    }
-
-
-    private static class FamilyResults {
-        List<Integer> individualsWithZeroParents;
-        List<Integer> individualsWithOneParent;
-
-        //getter,setters
-    }
-
-    public static void main(String[] args) {
-        // { parent, child }
-        int[][] parentChildPairs = new int[][]{
-                {1, 3}, {2, 3}, {3, 6}, {5, 6}, {5, 7},
-                {4, 5}, {4, 8}, {8, 10}
-        };
-
-        FamilyResults results = findNodesWithZeroAndOneParents(parentChildPairs);
-
-        System.out.println(results.individualsWithZeroParents);
-
-        System.out.println(results.individualsWithOneParent);
-
-
-        System.out.println(hasCommonAncestor(parentChildPairs, 6, 8));
-        System.out.println(hasCommonAncestor(parentChildPairs, 1, 3));
-        System.out.println(hasCommonAncestor(parentChildPairs, 5, 8));
-        System.out.println(hasCommonAncestor(parentChildPairs, 8, 3));
-
-    }
 }
-
-
-
