081970c70b16ca857b059e2cf2b9de523f071005
Prasad Shindikar <prasad.shindikar@nordstrom.com>
2019-04-30 15:55:27 -0700

    try creation of a mirror tree

--- src/main/java/karate/chops/trees/CreateMirrorBT.java
+++ src/main/java/karate/chops/trees/CreateMirrorBT.java
@@ -2,29 +2,28 @@ package karate.chops.trees;
 
 public class CreateMirrorBT {
 
-	public static void main(String[] args) {
-		BinaryTreeNode root = BinaryTree.createPerfectBinaryTree();
-		BinaryTreeNode mirror = createMirror(root);
+    public static void main(String[] args) {
+        BinaryTreeNode root = BinaryTree.createLeftSkewedTree();
+        BinaryTreeNode mirror = createMirror(root);
+        printTrees(root, mirror);
 
-		BinaryTree.printTreeInorder(root);
-		System.out.println("");
-		BinaryTree.printTreeInorder(mirror);
-	}
+        root = BinaryTree.createPerfectBinaryTree();
+        mirror = createMirror(root);
+        printTrees(root, mirror);
+    }
 
-//	private static BinaryTreeNode createMirror(BinaryTreeNode n) {
-//		if (n == null)
-//			return null;
-//
-//		BinaryTreeNode mirror = new BinaryTreeNode(n.value, null, null);
-//		mirror.right = createMirror(n.left);
-//		mirror.left = createMirror(n.right);
-//		return mirror;
-//
-//	}
+    private static BinaryTreeNode createMirror(BinaryTreeNode root) {
+        if (root == null)
+            return null;
+        return new BinaryTreeNode(root.value, createMirror(root.right), createMirror(root.left));
+    }
 
-	private static BinaryTreeNode createMirror(BinaryTreeNode root) {
-		if (root == null)
-			return null;
-		return new BinaryTreeNode(root.value, createMirror(root.right), createMirror(root.left));
-	}
+    private static void printTrees(BinaryTreeNode root, BinaryTreeNode mirror) {
+        System.out.print("Original Tree (Inorder): ");
+        BinaryTree.printTreeInorder(root);
+        System.out.println("");
+        System.out.print("Mirror Tree (Inorder): ");
+        BinaryTree.printTreeInorder(mirror);
+        System.out.println("");
+    }
 }
