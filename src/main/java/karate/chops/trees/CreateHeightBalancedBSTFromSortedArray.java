405d047f39dc367dadf766e2966a21db0186dc26
Prasad Shindikar <prasad.shindikar@nordstrom.com>
2019-05-23 21:22:12 -0700

    try creation of a bst tree from sorted array

--- src/main/java/karate/chops/trees/CreateHeightBalancedBSTFromSortedArray.java
+++ src/main/java/karate/chops/trees/CreateHeightBalancedBSTFromSortedArray.java
@@ -1,22 +1,22 @@
 package karate.chops.trees;
 
 public class CreateHeightBalancedBSTFromSortedArray {
+    public static void main(String[] args) {
+        BinaryTree.printTreeInorder(createHeightBalancedBST(new int[]{8, 10, 15, 23, 32, 48, 65}));
+        System.out.println("");
+        BinaryTree.printTreeInorder(createHeightBalancedBST(new int[]{5, 8, 10, 15, 23, 32, 48, 65}));
+    }
 
-	public static void main(String[] args) {
-		BinaryTree.printTreeInorder(create(new int[] { 8, 10, 15, 23, 32, 48, 65 }));
-		System.out.println("");
-		BinaryTree.printTreeInorder(create(new int[] { 5, 8, 10, 15, 23, 32, 48, 65 }));
-	}
+    private static BinaryTreeNode createHeightBalancedBST(int[] elements) {
+        if (elements == null || elements.length == 0)
+            return null;
+        return helper(0, elements.length - 1, elements);
+    }
 
-	private static BinaryTreeNode create(int[] A) {
-		return helper(A, 0, A.length - 1);
-	}
-
-	private static BinaryTreeNode helper(int[] A, int low, int high) {
-		if (low > high)
-			return null;
-		int mid = low + (high - low) / 2;
-		BinaryTreeNode n = new BinaryTreeNode(A[mid], helper(A, low, mid - 1), helper(A, mid + 1, high));
-		return n;
-	}
+    private static BinaryTreeNode helper(int low, int high, int[] elements) {
+        if (low > high)
+            return null;
+        int mid = low + ((high - low) / 2);
+        return new BinaryTreeNode(elements[mid], helper(low, mid - 1, elements), helper(mid + 1, high, elements));
+    }
 }
