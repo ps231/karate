6a9a1348b49f846ec4f6c4394d73a355b83f9b77
Prasad Shindikar <prasad.shindikar@nordstrom.com>
2019-06-04 21:15:26 -0700

    find the running median given a stream of integers

---
+++ src/main/java/karate/chops/heaps/MedianFinder.java
@@ -0,0 +1,39 @@
+package karate.chops.heaps;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class MedianFinder {
+
+    private PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    private PriorityQueue<Integer> maxHeap = new PriorityQueue<>(new Comparator<Integer>() {
+        public int compare(Integer i1, Integer i2) {
+            return Integer.compare(i2, i1);
+        }
+    });
+
+    public void add(int number) {
+        minHeap.offer(number);
+        if (minHeap.size() - maxHeap.size() > 1)
+            maxHeap.offer(minHeap.poll());
+    }
+
+    public double getMedian() {
+        if (minHeap.size() - maxHeap.size() == 1)
+            return minHeap.peek();
+        if (minHeap.size() == maxHeap.size())
+            return (minHeap.peek() + maxHeap.peek()) / 2.0;
+        throw new IllegalStateException("invalid state");
+    }
+
+    public static void main(String[] args) {
+        MedianFinder mf = new MedianFinder();
+        mf.add(100);
+        mf.add(90);
+        mf.add(105);
+        mf.add(80);
+        mf.add(115);
+        System.out.println(mf.getMedian());
+    }
+}
+
