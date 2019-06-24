a2801410df87c9b995bb934f2530c134aeaa0831
Prasad Shindikar <prasad.shindikar@nordstrom.com>
2019-06-24 06:14:00 -0700

    add more test cases and fix the logic

--- src/main/java/karate/chops/trees/SuccessorInInorderBT.java
+++ src/main/java/karate/chops/trees/SuccessorInInorderBT.java
@@ -3,41 +3,52 @@ package karate.chops.trees;
 import java.util.Stack;
 
 public class SuccessorInInorderBT {
-	
-	public static void main(String...strings) {
-		BinaryTreeNode successor = findSuccessor(BinaryTree.createPerfectBinaryTree(), 4);
-	}
+    public static void main(String... strings) {
+        printSuccessor(findSuccessor(BinaryTree.createPerfectBinaryTree(), 1));
+        printSuccessor(findSuccessor(BinaryTree.createPerfectBinaryTree(), 2));
+        printSuccessor(findSuccessor(BinaryTree.createPerfectBinaryTree(), 3));
+        printSuccessor(findSuccessor(BinaryTree.createPerfectBinaryTree(), 4));
+        printSuccessor(findSuccessor(BinaryTree.createPerfectBinaryTree(), 5));
+        printSuccessor(findSuccessor(BinaryTree.createPerfectBinaryTree(), 6));
+        printSuccessor(findSuccessor(BinaryTree.createPerfectBinaryTree(), 7));
+        printSuccessor(findSuccessor(BinaryTree.createPerfectBinaryTree(), 14));
+        printSuccessor(findSuccessor(null, 14));
+    }
 
-	private static BinaryTreeNode findSuccessor(BinaryTreeNode root, int n) {
-		// TODO Auto-generated method stub
-		
-		Stack<BinaryTreeNode> stack = new Stack<BinaryTreeNode>();
-		
-		if(root==null) return null;
-		boolean successorFound = false;
-		
-		while(!stack.isEmpty() || root != null) {
-		while(root.left!=null) {
-			stack.push(root);
-			root = root.left;
-		}
-		
-		if(n==root.value)
-			successorFound = true;
-		
-		if(root.right == null) {
-			
-				root = stack.pop();
-				
-				if(n==root.value)
-					successorFound = true;
-			
-		}
-		
-		if(root.right!=null)
-			root = root.right;
-		}
-		return null;
-	}
+    private static void printSuccessor(BinaryTreeNode node) {
+        if (node == null)
+            System.out.println("No successor defined, or input node could not be found");
+        else
+            System.out.println(node.value);
+    }
 
+    private static BinaryTreeNode findSuccessor(BinaryTreeNode root, int n) {
+        if (root == null)
+            return null;
+        Stack<BinaryTreeNode> stack = new Stack<>();
+        while (root.value != n) {
+            if (root.left != null) {
+                stack.push(root);
+                root = root.left;
+            } else if (root.right != null) {
+                root = root.right;
+            } else {
+                if (stack.isEmpty())
+                    return null;
+                root = stack.pop();
+                if (root.right != null)
+                    root = root.right;
+            }
+        }
+
+        if (root.right != null) {
+            root = root.right;
+            while (root.left != null)
+                root = root.left;
+            return root;
+        }
+        if (!stack.isEmpty())
+            return stack.pop();
+        return null;
+    }
 }
