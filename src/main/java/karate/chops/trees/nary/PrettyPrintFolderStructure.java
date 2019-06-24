7ac868a6b28286dbe2f66e1a5b33175649898c0c
Prasad Shindikar <prasad.shindikar@nordstrom.com>
2019-06-23 22:24:20 -0700

    pretty print n-ary folder structure

---
+++ src/main/java/karate/chops/trees/nary/PrettyPrintFolderStructure.java
@@ -0,0 +1,31 @@
+package karate.chops.trees.nary;
+
+public class PrettyPrintFolderStructure {
+    private static String WHITE_SPACE = "  ";
+
+    public static void main(String[] args) {
+        prettyPrint(NaryTreeHelper.createFolderStructure());
+    }
+
+    private static void prettyPrint(NaryNode root) {
+        helper(root, 0);
+    }
+
+    private static void helper(NaryNode n, int level) {
+        if (n == null)
+            return;
+        print(n.val, level);
+        if (n.children != null && n.children.size() != 0) {
+            for (NaryNode child : n.children) {
+                helper(child, level + 1);
+            }
+        }
+    }
+
+    private static void print(String value, int level) {
+        for (int i = 0; i < level; i++) {
+            System.out.print(WHITE_SPACE);
+        }
+        System.out.println(value);
+    }
+}
