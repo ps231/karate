f222def6b1c888d10db0470c69f65491f2f55850
Prasad Shindikar <prasad.shindikar@nordstrom.com>
2019-06-27 13:29:52 -0700

    attempt root to leaf paths for a specific sum

--- src/main/java/karate/chops/trees/PrintRootToLeafPathsForSpecificSum.java
+++ src/main/java/karate/chops/trees/PrintRootToLeafPathsForSpecificSum.java
@@ -3,28 +3,29 @@ package karate.chops.trees;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PrintRootToSumPathsForSpecificSum {
+public class PrintRootToLeafPathsForSpecificSum {
     public static void main(String[] args) {
         BinaryTreeNode n = BinaryTree.createPerfectBinaryTree();
         n.left.left.value = 7;
         printPathThatHasExactSum(n, 10);
         printPathThatHasExactSum(BinaryTree.createPerfectBinaryTree(), 11);
         printPathThatHasExactSum(BinaryTree.createPerfectBinaryTree(), 6);
+        printPathThatHasExactSum(null, 6);
     }
 
     private static void printPathThatHasExactSum(BinaryTreeNode n, int sum) {
         List<String> result = new ArrayList<>();
-        helper(n, sum, "", result);
+        helper(n, sum, result, "");
         System.out.println("Paths equal to sum: " + result.size());
         result.stream().forEach(System.out::println);
     }
 
-    private static void helper(BinaryTreeNode n, int sum, String intermediate, List<String> result) {
-        if (n == null)
+    private static void helper(BinaryTreeNode root, int remainingSum, List<String> paths, String intermediatePath) {
+        if (root == null)
             return;
-        if (n.left == null && n.right == null && n.value == sum)
-            result.add(intermediate + " " + n.value);
-        helper(n.left, sum - n.value, intermediate + " " + n.value, result);
-        helper(n.right, sum - n.value, intermediate + " " + n.value, result);
+        helper(root.left, remainingSum - root.value, paths, intermediatePath + " " + root.value);
+        helper(root.right, remainingSum - root.value, paths, intermediatePath + " " + root.value);
+        if (root.left == null && root.right == null && root.value == remainingSum)
+            paths.add(intermediatePath + " " + root.value);
     }
 }
