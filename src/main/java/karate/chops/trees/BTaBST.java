e69137abdccfde1d3ea7869589d725e964f26811
Prasad Shindikar <pshindik@amazon.com>
2019-11-07 21:56:04 -0800

    try if bt is a bst

--- src/main/java/karate/chops/trees/BTaBST.java
+++ src/main/java/karate/chops/trees/BTaBST.java
@@ -2,31 +2,17 @@ package karate.chops.trees;
 
 public class BTaBST {
 
-	public static void main(String...strings ) {
-		System.out.println(isBST(CreateBST.createBinarySearchTree(), Integer.MIN_VALUE, Integer.MAX_VALUE));
-		System.out.println(isBST(CreateBST.createAnotherBinaryTree(), Integer.MIN_VALUE, Integer.MAX_VALUE));
-		System.out.println(isBST(BinaryTree.createBalancedTree(), Integer.MIN_VALUE, Integer.MAX_VALUE));
-	}
+    public static void main(String... strings) {
+        System.out.println(isBST(CreateBST.createBinarySearchTree(), Integer.MIN_VALUE, Integer.MAX_VALUE));
+        System.out.println(isBST(CreateBST.createAnotherBinaryTree(), Integer.MIN_VALUE, Integer.MAX_VALUE));
+        System.out.println(isBST(BinaryTree.createBalancedTree(), Integer.MIN_VALUE, Integer.MAX_VALUE));
+    }
 
-//	private static boolean isBST(BinaryTreeNode n, int lowerBound, int upperBound) {
-//		if(n==null) return true;
-//		
-//		if(n.value > upperBound || n.value < lowerBound) return false;
-//		
-//		return n.value < upperBound
-//				&& n.value > lowerBound
-//				&& isBST(n.left, lowerBound, n.value)
-//				&& isBST(n.right, n.value, Math.max(n.value, upperBound));
-//		
-//	}
-
-	private static boolean isBST(BinaryTreeNode root, int min, int max) {
-		if(root==null)
-			return true;
-		
-		boolean left = isBST(root.left,min, root.value);
-		boolean right = isBST(root.right, root.value, max);
-		
-		return left&&right&&root.value>min&&root.value<max;
-	}
+    private static boolean isBST(BinaryTreeNode root, int min, int max) {
+        if (root == null)
+            return true;
+        if (root.value < min || root.value > max)
+            return false;
+        return isBST(root.left, min, root.value) && isBST(root.right, root.value, max);
+    }
 }
