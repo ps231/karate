8ed66ad26d1da8710ca2091294002c88a86c7f45
Prasad Shindikar <pshindik@amazon.com>
2019-11-10 12:38:22 -0800

    find elem in rotated sorted array

--- src/main/java/karate/chops/searching/FindElemInRotatedSortedArray.java
+++ src/main/java/karate/chops/searching/FindElemInRotatedSortedArray.java
@@ -1,53 +1,47 @@
 package karate.chops.searching;
 
 public class FindElemInRotatedSortedArray {
-	public static void main(String[] args) {
-		System.out.println(findElemInRotatedSortedArray(new int[] { 35, 38, 43, 5, 12, 17, 19 }, 19));
-		System.out.println(findElemInRotatedSortedArray(new int[] { 35, 38, 43, 5, 12, 17, 19 }, 18));
-		System.out.println(findElemInRotatedSortedArray(new int[] { 17, 19, 35, 38, 43, 5, 12 }, 19));
-		System.out.println(findElemInRotatedSortedArray(new int[] { 17, 19, 35, 38, 43, 5, 12 }, 18));
-		System.out.println(findElemInRotatedSortedArray(new int[] { 43, 5, 12, 17, 19, 35, 38 }, 19));
-		System.out.println(findElemInRotatedSortedArray(new int[] { 43, 5, 12, 17, 19, 35, 38 }, 18));
-		System.out.println(findElemInRotatedSortedArray(new int[] { 43, 5, 12, 17, 19, 35, 38 }, 43));
-		System.out.println(findElemInRotatedSortedArray(new int[] { 43, 5, 12, 17, 19, 35, 38 }, 38));
-	}
+    public static void main(String[] args) {
+        System.out.println(findElemInRotatedSortedArray(new int[]{35, 38, 43, 5, 12, 17, 19}, 19));
+        System.out.println(findElemInRotatedSortedArray(new int[]{19, 35, 38, 43, 5, 12, 17}, 19));
+        System.out.println(findElemInRotatedSortedArray(new int[]{17, 19, 35, 38, 43, 5, 12}, 19));
+        System.out.println(findElemInRotatedSortedArray(new int[]{12, 17, 19, 35, 38, 43, 5}, 19));
+        System.out.println(findElemInRotatedSortedArray(new int[]{5, 12, 17, 19, 35, 38, 43}, 19));
+        System.out.println(findElemInRotatedSortedArray(new int[]{43, 5, 12, 17, 19, 35, 38}, 19));
+        System.out.println(findElemInRotatedSortedArray(new int[]{38, 43, 5, 12, 17, 19, 35}, 19));
+        System.out.println(findElemInRotatedSortedArray(new int[]{35, 38, 43, 5, 12, 17, 19}, 18));
+        System.out.println(findElemInRotatedSortedArray(new int[]{19, 35, 38, 43, 5, 12, 17}, 18));
+        System.out.println(findElemInRotatedSortedArray(new int[]{17, 19, 35, 38, 43, 5, 12}, 18));
+        System.out.println(findElemInRotatedSortedArray(new int[]{12, 17, 19, 35, 38, 43, 5}, 18));
+        System.out.println(findElemInRotatedSortedArray(new int[]{5, 12, 17, 19, 35, 38, 43}, 18));
+        System.out.println(findElemInRotatedSortedArray(new int[]{43, 5, 12, 17, 19, 35, 39}, 18));
+        System.out.println(findElemInRotatedSortedArray(new int[]{38, 43, 5, 12, 17, 19, 35}, 18));
+    }
 
-	private static boolean findElemInRotatedSortedArray(int[] A, int elem) {
-		if (A == null || A.length == 0)
-			return false;
-		int low = 0, high = A.length - 1;
-		while (low < high) {
-			int mid = low + (high - low) / 2;
-			if (A[mid] > A[high])
-				low = mid + 1;
-			else
-				high = mid;
-		}
-		int[] temp = null;
-		if (low == 0)
-			temp = A;
-		else {
-			int[] A1 = new int[low];
-			System.arraycopy(A, 0, A1, 0, low);
-			int[] A2 = new int[A.length - low];
-			System.arraycopy(A, low, A2, 0, A.length - low);
-			if (elem < A1[0])
-				temp = A2;
-			else
-				temp = A1;
-		}
+    private static boolean findElemInRotatedSortedArray(int[] A, int num) {
+        if (A == null || A.length == 0)
+            return false;
 
-		low = 0;
-		high = temp.length - 1;
-		while (low <= high) {
-			int mid = low + (high - low) / 2;
-			if (temp[mid] == elem)
-				return true;
-			else if (temp[mid] > elem)
-				high = mid - 1;
-			else
-				low = mid + 1;
-		}
-		return false;
-	}
+        int low = 0;
+        int high = A.length - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) / 2);
+
+            if (A[mid] == num)
+                return true;
+
+            if (A[high] > A[mid]) {
+                if (A[mid] < num && A[high] >= num)
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            } else {
+                if (A[mid] > num && A[low] <= num)
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }
+        }
+        return false;
+    }
 }
