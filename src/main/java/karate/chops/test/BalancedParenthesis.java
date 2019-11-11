1338622523d0e30359c853af4d396bf393227e55
Prasad Shindikar <pshindik@amazon.com>
2019-11-10 16:05:32 -0800

    try balanced parenthesis

--- src/main/java/karate/chops/test/BalancedParenthesis.java
+++ src/main/java/karate/chops/test/BalancedParenthesis.java
@@ -4,43 +4,43 @@ import java.util.Stack;
 
 public class BalancedParenthesis {
 
-	public static void main(String[] args) {
-		System.out.println(areParenthesisBalanced("[(])"));
-		System.out.println(areParenthesisBalanced("[[[({{}})(){}[]]](({}[]))]"));
-		System.out.println(areParenthesisBalanced("[[](){}"));
-		System.out.println(areParenthesisBalanced("}}"));
-		System.out.println(areParenthesisBalanced("{}"));
-		System.out.println(areParenthesisBalanced("{)"));
-	}
+    public static void main(String[] args) {
+        System.out.println(areParenthesisBalanced("[(])"));
+        System.out.println(areParenthesisBalanced("[[[({{}})(){}[]]](({}[]))]"));
+        System.out.println(areParenthesisBalanced("[[](){}"));
+        System.out.println(areParenthesisBalanced("}}"));
+        System.out.println(areParenthesisBalanced("{}"));
+        System.out.println(areParenthesisBalanced("{)"));
+        System.out.println(areParenthesisBalanced("{}a"));
+    }
 
-	private static boolean areParenthesisBalanced(String expression) {
-		if (expression == null || expression.length() == 0)
-			throw new IllegalArgumentException("Expression must be provided");
+    private static boolean areParenthesisBalanced(String input) {
+        if (input == null || input.length() == 0)
+            throw new IllegalArgumentException("invalid input");
 
-		Stack<Character> stack = new Stack<Character>();
-		for (int i = 0; i < expression.length(); i++) {
-			char c = expression.charAt(i);
-			if (c == '(' || c == '[' || c == '{')
-				stack.push(c);
-			else if (c == ')' || c == ']' || c == '}') {
-				switch (c) {
-				case ')':
-					if (stack.size() == 0 || stack.pop() != '(')
-						return false;
-					break;
-				case ']':
-					if (stack.size() == 0 || stack.pop() != '[')
-						return false;
-					break;
-				case '}':
-					if (stack.size() == 0 || stack.pop() != '{')
-						return false;
-					break;
-				}
-			} else
-				return false;
-		}
-		return stack.size() == 0;
-	}
-
+        Stack<Character> s = new Stack<>();
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (c == '[' || c == '{' || c == '(')
+                s.push(c);
+            else if (c == ']' || c == '}' || c == ')') {
+                switch (c) {
+                    case ']':
+                        if (s.isEmpty() || s.pop() != '[')
+                            return false;
+                        break;
+                    case '}':
+                        if (s.isEmpty() || s.pop() != '{')
+                            return false;
+                        break;
+                    case ')':
+                        if (s.isEmpty() || s.pop() != '(')
+                            return false;
+                        break;
+                }
+            } else
+                throw new IllegalArgumentException("invalid input: " + c);
+        }
+        return s.isEmpty();
+    }
 }
